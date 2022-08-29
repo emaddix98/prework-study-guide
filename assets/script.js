6ef03908bbdc080c4d21bd70d4315f223dbd0a2d
Ethan <Ethan@ethans-mbp-7.attlocal.net>
2022-08-29 12:07:22 -0500

    added JavaScript functions

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,27 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+var Randomtopic = [Math.floor(Math.random() * topics.length)];
+function ListTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
 
-/*
-if (topic === 'HTML') {
-    console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-    console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-    console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-    console.log("Let's study JavaScript!");
-} else{
-    console.log('Please try again!');
+
+function SelectTopic(){
+    if (topics[Randomtopic] === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (topics[Randomtopic] === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (topics[Randomtopic] === 'Git') {
+        console.log("Let's study Git!");
+    } else if (topics[Randomtopic] === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else{
+        console.log('Please try again!');
+    }
 }
-*/
+
+console.log("Here are the topics we learned through prework");
+ListTopics();
+console.log("which topic should we study first?");
+SelectTopic();
